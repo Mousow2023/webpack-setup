fe99ec6bfa97b014b2d2d0accff4425e7d2a3195
Moustapha SOW <mousow123@gmail.com>
2025-01-02 14:55:07 +0000

    Setup eslint and prettier

---
+++ src/test.js
@@ -0,0 +1,18 @@
+const foo = 5;
+
+function testFunction(param1, param2) {
+  return param1 + param2;
+}
+
+const add = (x) => x + 1;
+
+const sum = 2 + 2;
+
+const person = { name: "John", age: 30 };
+
+const name = "Jhon";
+
+const arr = [
+  1,
+  2, // Error: Trailing comma is forbidden
+];
